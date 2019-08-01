6d3dba81d37a2395d825a556bd00b483d60eacd9
bradcgarner <brad@bradgarner.com>
2019-08-01 13:42:31 -0400

    added logo and list of projects

--- components/home/landing.js
+++ components/home/landing.js
@@ -1,15 +1,35 @@
 import content             from '../../helpers/content';
 import { accentFontColor } from '../../helpers/common-styles';
 import Building from './building-multiple-types';
+import GRDetLogo from '../graphics/greenroofdetention';
+import Link from 'next/link';
 
 export default function Landing(props) {
+
+  const links = [
+    'polderdak',
+    'purple-roof',
+    'hydrotech',
+    'hydroventive',
+    'haybase',
+    'custom'
+  ].map((l,i)=>{
+    return <Link key={i} href={`/${l}`}>{l}</Link>
+  });
 
   return <header className='landing'>
     <div className='cover'>
       <div className='building-container'>
+        <GRDetLogo />
+      </div>
+      <div className='building-container'>
         <Building/>
         <p className='absolut'>X</p>
       </div>
+    </div>
+    <div className='products'>
+      <p>These products will be shown</p>
+      {links}
     </div>
     <style jsx>{`
       .landing,
@@ -17,6 +37,9 @@ export default function Landing(props) {
         height: 100vh;
         width: 100vw;
         min-height: 440px;
+      }
+      .landing {
+        flex-direction: column;
       }
       .building-container {
         display: block;
@@ -38,6 +61,9 @@ export default function Landing(props) {
         align-items: center;
         justify-content: center;
         z-index: 2;
+      }
+      .products {
+        flex-direction: column;
       }
     `}</style>
   </header>
